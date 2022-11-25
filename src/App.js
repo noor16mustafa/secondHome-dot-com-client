07a26509d11901a82fd02d0e0747c281ddfb2952
noor16mustafa <c173212@ugrad.iiuc.ac.bd>
2022-11-26 01:26:20 +0600

    registration added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { router } from './routes/router';
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="">
       <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
     </div>
   );
 }
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const Register = () => {
+    const { createUser, updateUser } = useContext(AuthContext);
+    const handleSignUp = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const role = form.role.value;
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                toast.success('Sign Up Successful');
+                const userInfo = {
+                    displayName: name
+                }
+                updateUser(userInfo)
+                    .then(() => {
+                        saveUser(name, email, role);
+                    })
+                    .catch(err => console.log(err));
+            })
+            .catch(error => {
+                console.log(error)
+
+            });
+    }
+
+    const saveUser = (name, email, role) => {
+        const user = { name, email, role };
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
     return (
         <div className="hero  bg-base-200 ">
             <div className="hero-content w-3/5">
 
-                <div className="card  w-1/2 shadow-2xl bg-base-100 mt-14 mb-14">
+                <div className="card  w-1/2 shadow-2xl bg-base-100 mt-14 mb-14 text-center">
                     <h1 className="text-4xl font-bold pt-10"> Sign Up First</h1>
                     <div className="card-body">
-                        <form>
+                        <form onSubmit={handleSignUp}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Full Name</span>
@@ -34,9 +79,9 @@ const Register = () => {
                                     <span className="label-text">Choose Your Category?</span>
 
                                 </label>
-                                <select className="select select-bordered">
-                                    <option selected>Buyer</option>
-                                    <option>Seller</option>
+                                <select name='role' className="select select-bordered">
+                                    <option value='Buyer' selected>Buyer</option>
+                                    <option value='Seller'>Seller</option>
 
                                 </select>
 
